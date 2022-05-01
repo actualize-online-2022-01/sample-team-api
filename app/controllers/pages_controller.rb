68738ce955e8d9250e1dde67d01cb78834b49615
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-05-01 10:22:18 -0500

    fix merge conflict

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
   def index
-<<<<<<< HEAD
     render json: {message: "Katherine, Casey, Patrick, Ismail"}
-=======
-    render json: {message: "Sponge Bob"}
->>>>>>> 303f429d3fd08c27b5f1d45ab7bacaa42f6d48a2
   end
 end
